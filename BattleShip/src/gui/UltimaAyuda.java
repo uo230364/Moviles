b95285b2ef8a7454f66a90ed0f00f479c9220eec
Maria <Maria@MeriPC>
2014-12-26 13:42:04 +0100

    TercerAyuda?

---
+++ BattleShip/src/gui/UltimaAyuda.java
@@ -0,0 +1,31 @@
+package gui;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.battleship.R;
+
+public class UltimaAyuda extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_ultima_ayuda);
+	}
+	
+	public void terceraAyuda(View view){
+		Intent mIntent=new Intent(this,TerceraAyuda.class);
+		startActivity(mIntent);
+		finish();
+	}
+	
+	@Override
+	public void onBackPressed() {
+		Intent mIntent=new Intent(this, TerceraAyuda.class);
+		startActivity(mIntent);
+		finish();
+		super.onBackPressed();
+	}
+}
