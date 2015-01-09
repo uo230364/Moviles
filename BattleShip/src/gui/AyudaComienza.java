8757f0e84c131a160f0bfe10fe2273bced1cf059
David <davidcasado@outlook.com>
2015-01-09 14:20:05 +0100

    Ayuda modificada para solo cambiar de layout y no de activity

--- BattleShip/src/gui/AyudaComienza.java
+++ BattleShip/src/gui/AyudaComienza.java
@@ -9,27 +9,66 @@ import com.example.battleship.R;
 
 public class AyudaComienza extends Activity {
 
+	private final static int PRIMERA_AYUDA = R.layout.activity_ayuda;
+	private final static int SEGUNDA_AYUDA = R.layout.activity_segunda_ayuda;
+	private final static int TERCERA_AYUDA = R.layout.activity_tercera_ayuda;
+	private final static int CUARTA_AYUDA = R.layout.activity_ultima_ayuda;
+	private static int currentLayout;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_ayuda);
 	}
-	
-	public void pantallaInicial(View view){
-    	finish();
+
+	public void atrasAyuda(View view) {
+		switch (currentLayout) {
+		case PRIMERA_AYUDA:
+			finish();
+			break;
+		case SEGUNDA_AYUDA:
+			setContentView(R.layout.activity_ayuda);
+			break;
+		case TERCERA_AYUDA:
+			setContentView(R.layout.activity_segunda_ayuda);
+			break;
+		case CUARTA_AYUDA:
+			setContentView(R.layout.activity_tercera_ayuda);
+			break;
+		}
 	}
-	
-	public void masAyuda(View view){
-		Intent mInten=new Intent(AyudaComienza.this,SegundaAyuda.class);
-		startActivity(mInten);
+
+	public void masAyuda(View view) {
+		// Intent mInten=new Intent(AyudaComienza.this,SegundaAyuda.class);
+		// startActivity(mInten);
+		// finish();
+		switch (currentLayout) {
+		case PRIMERA_AYUDA:
+			setContentView(R.layout.activity_segunda_ayuda);
+			break;
+		case SEGUNDA_AYUDA:
+			setContentView(R.layout.activity_tercera_ayuda);
+			break;
+		case TERCERA_AYUDA:
+			setContentView(R.layout.activity_ultima_ayuda);
+			break;
+		case CUARTA_AYUDA:
+			setContentView(R.layout.activity_ayuda);
+			break;
+		}
+
+	}
+
+	@Override
+	public void onBackPressed() {
 		finish();
+		super.onBackPressed();
 	}
-	
-    @Override
-    public void onBackPressed() {
-    	finish();
-    	super.onBackPressed();
-    }
-    
-    
+
+	@Override
+	public void setContentView(int layoutResID) {
+		currentLayout = layoutResID;
+		super.setContentView(layoutResID);
+	}
+
 }
